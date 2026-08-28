a390b0a6cc24995e1253a4663e97f957e66dda86
Sindre Løvlie Haugen <sindre.haugen@gmail.com>
2026-08-28 20:01:42 +0200

    test: ts-ignore gitignored json fixtures

--- app/src/exchange/easyschematic/read.test.ts
+++ app/src/exchange/easyschematic/read.test.ts
@@ -59,8 +59,8 @@
  */
 
 import { describe, expect, it } from 'vitest';
-import fixtureGymmen from '../../../tests/fixtures/av-fasit/AV_U1A21.easyschematic.json';
-import fixtureStudio from '../../../tests/fixtures/av-fasit/AV_H3B19.easyschematic.json';
+// @ts-ignore`nimport fixtureGymmen from '../../../tests/fixtures/av-fasit/AV_U1A21.easyschematic.json';
+// @ts-ignore`nimport fixtureStudio from '../../../tests/fixtures/av-fasit/AV_H3B19.easyschematic.json';
 import { readEasySchematic } from './read';
 import { DesignDocumentSchema } from '../../model/schema';
 

2ce3f6622cbf1b0171e2c1aafe7a0d6e6ad693f2
Sindre Løvlie Haugen <sindre.haugen@gmail.com>
2026-08-31 09:36:54 +0200

    fix(canvas): missing xyflow css import

--- app/src/main.tsx
+++ app/src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client'
 import { AppShell } from './shell'
 import './locales/i18n'
 import './theme/theme.css'
+import '@xyflow/react/dist/style.css'
 
 const container = document.getElementById('root')
 if (container) {
@@ -13,3 +14,4 @@ if (container) {
     </StrictMode>
   )
 }
+
